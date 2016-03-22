8af11c08f3fbbe9db7ed730b8ee4cbb35c02cb13
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-03-22 14:41:12 +0000

    Added property to return iris Cube from HoloCube if available

--- holocube/element/cube.py
+++ holocube/element/cube.py
@@ -26,6 +26,14 @@ class HoloCube(Table):
     group = param.String(default='HoloCube')
 
     @property
+    def cube(self):
+        " Returns the underlying iris Cube if available. "
+        if issubclass(self.interface, CubeConversion):
+            return self.data
+        else:
+            return None
+
+    @property
     def to(self):
         """
         Property to create a conversion table with methods to convert
